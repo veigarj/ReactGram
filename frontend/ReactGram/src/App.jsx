c063f2d3a1e0ae8d33581f19eb234ee9ff7e87a2
Glauco Veiga <veiga.cg@gmail.com>
2023-04-26 18:58:52 -0300

    Navbar e Footer

--- frontend/ReactGram/src/App.jsx
+++ frontend/ReactGram/src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
 
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+
+// Components
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+// Pages
+import Home from './pages/Home/Home'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+
 function App() {
 
   return (
     <div>
-      <h1>ReactGram</h1>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/' element={<Login />}/>
+          <Route path='/' element={<Register />}/>
+        </Routes>
+          <Footer />
+      </BrowserRouter>
       
     </div>
   )
---
+++ frontend/ReactGram/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import styles from './Navbar.module.css'
+
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
