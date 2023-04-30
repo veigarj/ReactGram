d3ff4f30f74cfca357bf34e3a82cf5b5fc17d071
Glauco Veiga <veiga.cg@gmail.com>
2023-04-29 21:02:27 -0300

    Auth form

--- frontend/ReactGram/src/pages/Auth/Register.jsx
+++ frontend/ReactGram/src/pages/Auth/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import styles  from "./Auth.module.css";
 // Components
 import { Link } from "react-router-dom";
 
@@ -13,33 +13,33 @@ const Register = () => {
 
 
   return (
-    <div>
+    <div className={styles.register}>
       <h2>ReactGram</h2>
       <p>Cadastre-se para ver as fotos  dos seus amigos.</p>
       <form onSubmit={handleSubmit}>
       <input
           type="text"
           placeholder="Nome"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
+          // onChange={(e) => setName(e.target.value)}
+          // value={name}
         />
         <input
           type="email"
           placeholder="E-mail"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
+          // onChange={(e) => setEmail(e.target.value)}
+          // value={email}
         />
         <input
           type="password"
           placeholder="Senha"
-          onChange={(e) => setPassword(e.target.value)}
-          value={password}
+          // onChange={(e) => setPassword(e.target.value)}
+          // value={password}
         />
         <input
           type="password"
           placeholder="Confirme a senha"
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          value={confirmPassword}
+          // onChange={(e) => setConfirmPassword(e.target.value)}
+          // value={confirmPassword}
         />
         <input type="submit" value="Cadastrar" />
       </form>
