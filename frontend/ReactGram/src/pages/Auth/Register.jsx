270caf2243f79e857e9bfacd2c5c7c10fc60f4ee
Glauco Veiga <veiga.cg@gmail.com>
2023-04-29 23:32:28 -0300

    Redux

--- frontend/ReactGram/src/pages/Auth/Register.jsx
+++ frontend/ReactGram/src/pages/Auth/Register.jsx
@@ -6,9 +6,23 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    console.log(user);
+
   }
 
 
@@ -20,26 +34,26 @@ const Register = () => {
       <input
           type="text"
           placeholder="Nome"
-          // onChange={(e) => setName(e.target.value)}
-          // value={name}
+          onChange={(e) => setName(e.target.value)}
+          value={name}
         />
         <input
           type="email"
           placeholder="E-mail"
-          // onChange={(e) => setEmail(e.target.value)}
-          // value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
         />
         <input
           type="password"
           placeholder="Senha"
-          // onChange={(e) => setPassword(e.target.value)}
-          // value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
         />
         <input
           type="password"
           placeholder="Confirme a senha"
-          // onChange={(e) => setConfirmPassword(e.target.value)}
-          // value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword}
         />
         <input type="submit" value="Cadastrar" />
       </form>
